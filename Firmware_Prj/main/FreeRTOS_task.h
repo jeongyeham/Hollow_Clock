fd09f64ecbb116866525503fb91341012ca311ba
jeongyeham <jeongyeham@foxmail.com>
2025-06-03 16:52:56 +0800

    feat(main): implement FreeRTOS tasks for step motor control and Wi-Fi initialization- Add step motor control task for handling stepper motor commands - Implement Wi-Fi initialization and smart config tasks - Create header file for declaring the new tasks

---
+++ Firmware_Prj/main/FreeRTOS_task.h
@@ -0,0 +1,12 @@
+//
+// Created by jeong on 2025/5/27.
+//
+#ifndef FREERTOS_TASK_H
+#define FREERTOS_TASK_H
+
+
+void step_motor_task(void *param);
+void initialise_wifi_task(void *pvParameters);
+void smartconfig_task(void *pvParameters);
+
+#endif //FREERTOS_TASK_H
