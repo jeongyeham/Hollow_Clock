17d5c4e17bc36c4dcce83788581c0a0c1bb59b0e
jeongyeham <jeongyeham@foxmail.com>
2025-06-03 16:52:39 +0800

    feat(components): add step_motor component header file
    - Create step_motor.h file for stepper motor control
    - Define stepper_cmd structure for motor command
    - Declare functions for stepper motor initialization and control

---
+++ Firmware_Prj/components/step_motor/include/step_motor.h
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2025 JeongYeham
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef STEP_MOTOR_H
+#define STEP_MOTOR_H
+
+#include <stdbool.h>
+
+typedef struct stepper_cmd
+{
+    int steps;
+    bool dir_cw;
+    int speed_us;
+} stepper_cmd_t;
+
+
+void stepper_driver_init(void);
+void step_motor_task(void* param);
+void stepper_rotate_angle(float degree, bool cw, int rpm);
+void stepper_set_motion(int steps, bool dir, int speed_us);
+
+#endif
